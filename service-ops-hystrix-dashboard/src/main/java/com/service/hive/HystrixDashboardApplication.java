c7b39bd33147a33f636e4f5e48df19932c89c574
kenly <likuan33@126.com>
2017-12-30 00:41:58 +0800

    hystrix dashboard join to eureka

--- service-ops-hystrix-dashboard/src/main/java/com/service/hive/HystrixDashboardApplication.java
+++ service-ops-hystrix-dashboard/src/main/java/com/service/hive/HystrixDashboardApplication.java
@@ -2,6 +2,7 @@ package com.service.hive;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 /**
@@ -10,6 +11,7 @@ import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboar
  */
 @SpringBootApplication
 @EnableHystrixDashboard
+@EnableDiscoveryClient
 public class HystrixDashboardApplication {
     public static void main(String[] args) {
         new SpringApplicationBuilder(HystrixDashboardApplication.class).web(true).run(args);
